acfb56f6e93288f82246fc656b0bfc8218e94de6
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2020-02-28 14:35:49 +0900

    showBbsControllerを作成しました

--- src/main/java/com/example/controller/ShowBbsController.java
+++ src/main/java/com/example/controller/ShowBbsController.java
@@ -1,5 +1,37 @@
 package com.example.controller;
 
-public class ShowBbsController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Article;
+import com.example.form.ArticleForm;
+import com.example.repository.ArticleRepository;
+
+public class ShowBbsController {
+	
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	@ModelAttribute
+	public ArticleForm setUpArticleForm() {
+		return new ArticleForm();
+	}
+	
+	/**
+	 * 記事,コメントを表示する.
+	 * 
+	 * @return 記事,コメント表示html
+	 */
+	@RequestMapping("")
+	public String index(Model model) {
+		//ここから中級課題
+		List<Article> articleList = articleRepository.findAll();
+		model.addAttribute("articleList", articleList);
+		
+		return "bbs/bbs";
+	}
 }
