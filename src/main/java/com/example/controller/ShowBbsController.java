38dbdb9b1e76318b8c3e9f504959285de59cae6b
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2020-05-22 21:58:05 +0900

    bbs-systemです

--- src/main/java/com/example/controller/ShowBbsController.java
+++ src/main/java/com/example/controller/ShowBbsController.java
@@ -5,13 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
-import com.example.form.ArticleForm;
 import com.example.mapper.ArticleMapper;
-import com.example.repository.ArticleRepository;
 
 @Controller
 @RequestMapping("")
